d2b16bc541a2e5806bbc548b296e0573e1c74fc7
Shubham Tandon <iamshubham316@gmail.com>
2023-11-24 19:32:18 +0530

    cleanup

--- src/components/SideIndex.js
+++ src/components/SideIndex.js
@@ -18,9 +18,7 @@ export default function(){
     1580.140625,
     1983.140625
   ])
-  // let i = document.getElementById('about-main').getBoundingClientRect();
-
-  // console.log(i.top);
+  
     return(<div id='index-main'>
       {tabArr.map((i,idx)=>{
         return <div id='tab' onClick={()=>{
